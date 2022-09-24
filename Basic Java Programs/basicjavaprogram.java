e73cda2647e3b8cec3922a3a6ff60dd27da3290f
Aman Khan <amankhann2023@gmail.com>
2022-09-24 21:21:44 +0530

    update

--- Basic Java Programs/basicjavaprogram.java
+++ Basic Java Programs/basicjavaprogram.java
@@ -3,8 +3,6 @@ package com.learnjava;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("Hello, World!");
         int a=13, b=12, sum;
         sum=a+b;
         System.out.println("Sum of these numbers is: "+sum);
