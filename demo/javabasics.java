e0f0394e15c64abdc67ab440c38498c38b175b5d
Aman Khan <amankhann2023@gmail.com>
2022-09-24 21:29:24 +0530

    update

--- demo/javabasics.java
+++ demo/javabasics.java
@@ -3,8 +3,10 @@ package com.learnjava;
 public class Main {
 
     public static void main(String[] args) {
-        int a=13, b=12, sum;
+        int a=13, b=12, sum, sub;
         sum=a+b;
+        sub=a-b;
         System.out.println("Sum of these numbers is: "+sum);
+        System.out.println("Substraction of these numbers is:"+sub);
     }
 }
