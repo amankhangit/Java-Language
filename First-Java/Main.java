8403285b01c9019cc0ea4b2e39b983b2376ca40d
Aman Khan <amankhann2023@gmail.com>
2022-10-03 20:02:48 +0530

    Main.java added

---
+++ First-Java/Main.java
@@ -0,0 +1,18 @@
+package com.learnjava;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.print("a = ");
+        int a = sc.nextInt();
+        System.out.print("b = ");
+        int b = sc.nextInt();
+        System.out.print("sum = ");
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
+
+
