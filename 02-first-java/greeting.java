92da0c337cf1f938465484fc9f547ea020a10f0d
Aman Khan <amankhann2023@gmail.com>
2022-10-04 22:09:29 +0530

    simpleinterest.java added

--- 02-first-java/greeting.java
+++ 02-first-java/greeting.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class greeting {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String name = input.nextLine();
-        System.out.println("Hello "+ name);
+
     }
 }
---
+++ 02-first-java/simpleinterest.java
@@ -0,0 +1,15 @@
+package com.learnjava;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Principal = ");
+        float p = input.nextInt();
+        System.out.print("Enter Time = ");
+        float t = input.nextInt();
+        System.out.print("Enter Rate = ");
+        float r = input.nextInt();
+        float si = (p*t*r)/100;
+        System.out.println("Simple Interest = " + si);
+    }
+}
