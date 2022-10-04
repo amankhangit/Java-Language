14f7b8ceb3360fca4c035e95415f76a75fe86e06
Aman Khan <amankhann2023@gmail.com>
2022-10-04 21:56:15 +0530

    greeting.java added

---
+++ 02-first-java/greeting.java
@@ -0,0 +1,9 @@
+package com.learnjava;
+import java.util.Scanner;
+public class greeting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("Hello "+ name);
+    }
+}
